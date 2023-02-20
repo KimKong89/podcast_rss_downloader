6dadb4d76dc6a87b1962797f161bb8ca0a9340eb
Kim Stedwell <Kim.Stedwell@gmx.de>
2023-02-20 13:02:08 +0100

    Script and other files added

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 "# podcast_rss_downloader" 
+simple script for downloading all podcast-episodes from an rss-feed. call like:
+python -u "https://feeds.buzzsprout.com/1940667.rss" -fp null_uhr_eins_all_episodes
---
+++ podcast_rss_downloader.py
@@ -0,0 +1,71 @@
+import requests
+from lxml import etree
+from rich.progress import track
+import argparse
+import os
+
+# Should work on all Buzzsprout-Sites. I have tested a couple of other rss-feeds as well.
+# Most testing was done with the page of my own podcast "Null Uhr Eins":
+# "https://feeds.buzzsprout.com/1940667.rss"
+# call like "python -u "https://feeds.buzzsprout.com/1940667.rss" -fp null_uhr_eins_all_episodes"
+
+
+def downloadEpisodesFromRSS(feed, fpath):
+    # Download rss-feed as xml
+    response = requests.get(feed)
+    content = response.content.decode()
+    content = content.replace('<?xml version="1.0" encoding="UTF-8" ?>', '')
+    content = content.replace('<?xml version="1.0" encoding="UTF-8"?>', '')
+    xml = etree.fromstring(content)
+
+    print(f'''found {len(xml.xpath('//item'))} episode(s) ''')
+
+    # loop over episodes
+    for item in track(xml.xpath('//item'), 'downloading'):
+        url = item.xpath('./enclosure')[0].attrib['url']
+
+        # for sorting purposes
+        season = item.xpath('./*[local-name() = "season"]')
+        if len(season) > 0:
+            season = season[0].text
+            season = str(int(season)).zfill(2)
+        else:
+            season = '00'
+
+        episode = item.xpath('./*[local-name() = "episode"]')
+        if len(episode) > 0:
+            episode = episode[0].text
+            episode = str(int(episode)).zfill(2)
+        else:
+            episode = '00'
+
+        # creating file-name, combining with path
+        fn = f'''S{season}E{episode}_{url.split('/')[-1]} '''
+        fp = os.path.join(fpath, fn)
+
+        # download
+        r = requests.get(url, allow_redirects=True)
+        open(fp, 'wb').write(r.content)
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-u', '--url', help='url to the rss-feed')
+    parser.add_argument('-fp', '--filepath', help='path to save downloaded files', default='.')
+    args = parser.parse_args()
+
+    if not args.url:
+        print('please provide rss-feed url (-u)')
+    else:
+        if not os.path.exists(args.filepath):
+            os.mkdir(args.filepath)
+
+        if args.url.startswith('"') or args.url.startswith("'"):
+            args.url = args.urls[1:-1]
+
+        downloadEpisodesFromRSS(args.url, args.filepath)
+        print('done')
+
+
+if __name__ == '__main__':
+    main()

482c06dc143a02b85503e82adc8d918b6e906e76
Kim Stedwell <Kim.Stedwell@gmx.de>
2023-02-20 14:50:26 +0100

    added functionality to download the n newest episodes

--- podcast_rss_downloader.py
+++ podcast_rss_downloader.py
@@ -10,18 +10,27 @@ import os
 # call like "python -u "https://feeds.buzzsprout.com/1940667.rss" -fp null_uhr_eins_all_episodes"
 
 
-def downloadEpisodesFromRSS(feed, fpath):
+def downloadEpisodesFromRSS(feed, fpath, nr):
     # Download rss-feed as xml
     response = requests.get(feed)
     content = response.content.decode()
     content = content.replace('<?xml version="1.0" encoding="UTF-8" ?>', '')
     content = content.replace('<?xml version="1.0" encoding="UTF-8"?>', '')
     xml = etree.fromstring(content)
+    matches = xml.xpath('//item')
 
-    print(f'''found {len(xml.xpath('//item'))} episode(s) ''')
+    print(f'''found {len(matches)} episode(s) ''')
+
+    # how many episodes to download
+    if nr is None or int(nr) > len(matches):
+        nr = len(matches)
+    else:
+        nr = int(nr)
+
+    print(f'''downloading {nr}/{len(matches)} episode(s) ''')
 
     # loop over episodes
-    for item in track(xml.xpath('//item'), 'downloading'):
+    for item in track(matches[0:nr], 'downloading'):
         url = item.xpath('./enclosure')[0].attrib['url']
 
         # for sorting purposes
@@ -52,6 +61,7 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('-u', '--url', help='url to the rss-feed')
     parser.add_argument('-fp', '--filepath', help='path to save downloaded files', default='.')
+    parser.add_argument('-n', '--nr', help='number of newest episode to download', default=None)
     args = parser.parse_args()
 
     if not args.url:
@@ -63,7 +73,7 @@ def main():
         if args.url.startswith('"') or args.url.startswith("'"):
             args.url = args.urls[1:-1]
 
-        downloadEpisodesFromRSS(args.url, args.filepath)
+        downloadEpisodesFromRSS(args.url, args.filepath, args.nr)
         print('done')
 
 
